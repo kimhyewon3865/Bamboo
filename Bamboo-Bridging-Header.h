64b3be4a418cfe41e2b9137ecc739b5ce9895339
박태현 <ppth0608@naver.com>
2015-12-30 14:08:15 +0900

    pods(TGCamera) 사진 라이브러리 추가, 브리징헤더파일 추가, PostViewController UI 작업중, 등록 ui및 기능 작업중 by taehyun.park

---
+++ Bamboo-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bamboo-Bridging-Header.h
+//  Bamboo
+//
+//  Created by 박태현 on 2015. 12. 29..
+//  Copyright © 2015년 ParkTaeHyun. All rights reserved.
+//
+
+#import <TGCameraViewController/TGCameraViewController.h>
---
+++ Bamboo/PostBoardViewController.swift
@@ -0,0 +1,79 @@
+//
+//  PostBoardViewController.swift
+//  Bamboo
+//
+//  Created by 박태현 on 2015. 12. 28..
+//  Copyright © 2015년 ParkTaeHyun. All rights reserved.
+//
+
+import UIKit
+
+class PostBoardViewController: UIViewController {
+    
+    //게시글 TextView
+    @IBOutlet weak var contentsTextView: UITextView!
+    //"속마음을 표현해보세요" 레이블
+    @IBOutlet weak var placeHolderLabel: UILabel!
+    //키보드 위 사진및 확성기 게시기능을 할수 있는 View
+    @IBOutlet weak var toolBoxView: UIView!
+    //툴박스 확성기 버튼
+    @IBOutlet weak var toolBoxNoticeButton: UIButton!
+    
+    //게시글 내용
+    var contents: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingContentsTextView()
+    }
+
+    // MARK: - Custom function
+    func settingContentsTextView() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyWillShow:"), name: UIKeyboardDidShowNotification, object: nil)
+        contentsTextView.becomeFirstResponder()
+        contentsTextView.autocorrectionType = .No
+    }
+    
+    func keyWillShow(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            if let keyboardSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+                self.toolBoxView.frame = CGRectMake(toolBoxView.frame.origin.x, toolBoxView.frame.origin.y + (keyboardSize.size.height * -1), toolBoxView.frame.size.width, toolBoxView.frame.size.height)
+            }
+        }
+    }
+    
+    // MARK: - IBAction function
+    @IBAction func closeButtonClicked(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func toolBoxCameraButtonClicked(sender: UIButton) {
+    }
+    
+    @IBAction func toolBoxNoticeButtonClicked(sender: UIButton) {
+    }
+    
+    @IBAction func toolBoxPostButtonClicked(sender: UIButton) {
+        
+    }
+}
+
+extension PostBoardViewController: UITextViewDelegate {
+    func textViewShouldBeginEditing(textView: UITextView) -> Bool {
+
+        return true
+    }
+    
+    func textViewShouldEndEditing(textView: UITextView) -> Bool {
+        return true
+    }
+    
+    func textViewDidChange(textView: UITextView) {
+        contents = contentsTextView.text
+        if contents != "" {
+            placeHolderLabel.hidden = true
+        } else {
+            placeHolderLabel.hidden = false
+        }
+    }
+}
