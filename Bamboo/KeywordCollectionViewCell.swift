7c38616652b46ef4aa54773b1774fb395a30a49e
김혜원 <kimhyewon@gimhyewonui-MacBook-Air.local>
2015-12-30 14:09:03 +0900

    detail class 생성. keywordCollectionviewCell생성 스토리보드, comment, detailviewcontroller 수정 detailViewController에서 response값이 없는 문제가 있음 by hyewon.Kim

---
+++ Bamboo/KeywordCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  KeywordCollectionViewCell.swift
+//  Bamboo
+//
+//  Created by 김혜원 on 2015. 12. 29..
+//  Copyright © 2015년 ParkTaeHyun. All rights reserved.
+//
+
+import UIKit
+
+class KeywordCollectionViewCell: UICollectionViewCell {
+    
+}
